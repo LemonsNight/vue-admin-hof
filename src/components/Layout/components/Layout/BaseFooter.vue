742f624e6bccef5a5431ee76a670b36e1f091042
Daiyousei <512469231@qq.com>
2023-02-06 18:06:25 +0800

    wip: 底部文字

--- src/components/Layout/components/Layout/BaseFooter.vue
+++ src/components/Layout/components/Layout/BaseFooter.vue
@@ -1,3 +1,5 @@
 <template>
-  <footer class="w-full h-20px"></footer>
+  <footer class="w-full h-20px text-xs text-blue-500 text-center">
+    <p>🧡Author: Daiyousei ❥(^_-)</p>
+  </footer>
 </template>
