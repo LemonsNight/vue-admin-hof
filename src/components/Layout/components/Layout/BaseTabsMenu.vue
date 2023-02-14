8a7fd1ef7b28d6bbebb3bb21066f0dbe8b516763
Daiyousei <512469231@qq.com>
2023-02-14 23:44:56 +0800

    wip: tabs

--- src/components/Layout/components/Layout/BaseTabsMenu.vue
+++ src/components/Layout/components/Layout/BaseTabsMenu.vue
@@ -22,31 +22,51 @@ const onActiveTab = (id: number) => {
     >
       <span :class="[ns.e('title')]">tabs-{{ item }}</span>
       <Icon :class="[ns.e('close')]" icon="material-symbols:close" size="10" />
-      <div class="absolute bottom-0 -right-10px bg-light-500">
+      <div
+        :class="ns.em('item', `border-${i}`)"
+        :key="`border-${i}`"
+        v-for="i in ['left', 'right']"
+      >
         <div
           v-if="item === tabsConfig.id"
-          style="border-radius: 0 0 0 3px"
-          class="w-10px h-10px bg-light-900"
-        ></div>
-      </div>
-      <div class="absolute bottom-0 -left-10px bg-light-500">
-        <div
-          v-if="item === tabsConfig.id"
-          style="border-radius: 0 0 3px 0"
-          class="w-10px h-10px bg-light-900"
-        ></div>
+          :class="ns.em('item', `${i}-round`)"
+        />
       </div>
     </li>
   </ul>
 </template>
 
 <style lang="scss" scoped>
+@mixin common-round($direction) {
+  @if $direction == left {
+    border-radius: 0 0 0 3px;
+  }
+  @if $direction == right {
+    border-radius: 0 0 3px 0;
+  }
+  @apply w-10px h-10px bg-light-900 z-10;
+}
+
 @include b(base-tabs-menu) {
   background: var(--tabs-bg-color);
   @apply relative flex items-center pt-2px pl-2 pr-2 text-xs select-none overflow-hidden;
 
   @include e(item) {
     @apply relative flex items-center whitespace-nowrap pl-10px pr-2px pt-1 pb-1 cursor-default;
+
+    @include m(border-right) {
+      @apply absolute bottom-0 -left-10px bg-light-500;
+    }
+
+    @include m(border-left) {
+      @apply absolute bottom-0 -right-10px bg-light-500;
+    }
+    @include m(left-round) {
+      @include common-round(left);
+    }
+    @include m(right-round) {
+      @include common-round(right);
+    }
   }
 
   @include e(title) {
@@ -54,10 +74,11 @@ const onActiveTab = (id: number) => {
   }
 
   @include e(close) {
+    @apply z-20;
     &:hover {
       background: var(--tabs-hover-close-bg-color);
       color: var(--tabs-hover-close-color);
-      padding: 4px;
+      @apply p-4px;
     }
   }
 
